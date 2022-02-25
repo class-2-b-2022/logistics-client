12a6f4759fc06cbf0852c6813ee7b40df437c4c0
KALASA_IVAN <moikalasa12@gmail.com>
2022-02-25 22:03:40 +0200

    modifications

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,6 +3,6 @@ package main;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello project");
+        System.out.println("Welcome to the logistics application");
     }
 }
