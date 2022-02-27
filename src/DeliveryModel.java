95a9b073b98158a19eb5179fb3edde203844c606
trezor13 <treasureharden14@gmail.com>
2022-02-27 00:40:19 -0800

    Delivery model simple console UI

--- src/DeliveryModel.java
+++ src/DeliveryModel.java
@@ -1,5 +1,28 @@
 public class DeliveryModel {
+    public static final String ANSI_BLUE = "\u001B[34m";
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static void WelcomeToDelivery(){
+        System.out.println(ANSI_BLUE +"\t\t\t\t                                                                                                       "+ANSI_RESET);
+        System.out.println(ANSI_BLUE +"\t\t\t\t         ----------------------------------                    "+ANSI_RESET);
+        System.out.println(ANSI_BLUE +"\t\t\t\t         |                                  |_____________                "+ANSI_RESET);
+        System.out.println(ANSI_BLUE +"\t\t\t\t         |                                  | |~~~~~~~~~| |            "+ANSI_RESET);
+        System.out.println(ANSI_BLUE +"\t\t\t\t         |    ______DELIVERY MODEL______    | |          | |           "+ANSI_RESET);
+        System.out.println(ANSI_BLUE +"\t\t\t\t         |                                  | |            | |         "+ANSI_RESET);
+        System.out.println(ANSI_BLUE +"\t\t\t\t         |                                  | | |           | |      "+ANSI_RESET);
+        System.out.println(ANSI_BLUE +"\t\t\t\t         |----------------------------------| |______________| |      "+ANSI_RESET);
+        System.out.println(ANSI_BLUE +"\t\t\t\t         |__________________________________|                  |   "+ANSI_RESET);
+        System.out.println(ANSI_BLUE +"\t\t\t\t         |                                                     |                       "+ANSI_RESET);
+        System.out.println(ANSI_BLUE +"\t\t\t\t         |       0 0 O                           0 0 0         |                       "+ANSI_RESET);
+        System.out.println(ANSI_BLUE +"\t\t\t\t         |-----0       0---------------------- 0       0-------|                                                                          "+ANSI_RESET);
+        System.out.println(ANSI_BLUE +"\t\t\t\t                 0 0 0                           0 0 0                                               "+ANSI_RESET);
+
+
+
+
+
+
+    }
     public static void main(String[] args) {
-        
+        WelcomeToDelivery();
     }
 }
