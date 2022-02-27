ac0e41f5a90a9778aca3195bdb11ea0b7eb21afc
trezor13 <treasureharden14@gmail.com>
2022-02-27 00:57:18 -0800

    simple update

--- src/DeliveryModel.java
+++ src/DeliveryModel.java
@@ -1,20 +1,23 @@
 public class DeliveryModel {
-    public static final String ANSI_BLUE = "\u001B[34m";
+    public static final String ANSI_BLUE = "\u001B[36m";
     public static final String ANSI_RESET = "\u001B[0m";
     public static void WelcomeToDelivery(){
-        System.out.println(ANSI_BLUE +"\t\t\t\t                                                                                                       "+ANSI_RESET);
-        System.out.println(ANSI_BLUE +"\t\t\t\t         ----------------------------------                    "+ANSI_RESET);
-        System.out.println(ANSI_BLUE +"\t\t\t\t         |                                  |_____________                "+ANSI_RESET);
-        System.out.println(ANSI_BLUE +"\t\t\t\t         |                                  | |~~~~~~~~~| |            "+ANSI_RESET);
-        System.out.println(ANSI_BLUE +"\t\t\t\t         |    ______DELIVERY MODEL______    | |          | |           "+ANSI_RESET);
-        System.out.println(ANSI_BLUE +"\t\t\t\t         |                                  | |            | |         "+ANSI_RESET);
-        System.out.println(ANSI_BLUE +"\t\t\t\t         |                                  | | |           | |      "+ANSI_RESET);
-        System.out.println(ANSI_BLUE +"\t\t\t\t         |----------------------------------| |______________| |      "+ANSI_RESET);
-        System.out.println(ANSI_BLUE +"\t\t\t\t         |__________________________________|                  |   "+ANSI_RESET);
-        System.out.println(ANSI_BLUE +"\t\t\t\t         |                                                     |                       "+ANSI_RESET);
-        System.out.println(ANSI_BLUE +"\t\t\t\t         |       0 0 O                           0 0 0         |                       "+ANSI_RESET);
-        System.out.println(ANSI_BLUE +"\t\t\t\t         |-----0       0---------------------- 0       0-------|                                                                          "+ANSI_RESET);
-        System.out.println(ANSI_BLUE +"\t\t\t\t                 0 0 0                           0 0 0                                               "+ANSI_RESET);
+        System.out.println(ANSI_BLUE +"\t\t\t\t                                                                   "+ANSI_RESET);
+        System.out.println(ANSI_BLUE +"\t\t\t\t                                                                   "+ANSI_RESET);
+        System.out.println(ANSI_BLUE +"\t\t\t\t                                                                   "+ANSI_RESET);
+        System.out.println(ANSI_BLUE +"\t\t\t\t                                                                   "+ANSI_RESET);
+        System.out.println(ANSI_BLUE +"\t\t\t\t         ----------------------------------                        "+ANSI_RESET);
+        System.out.println(ANSI_BLUE +"\t\t\t\t         |                                  |_____________         "+ANSI_RESET);
+        System.out.println(ANSI_BLUE +"\t\t\t\t         |                                  | |~~~~~~~~~| |        "+ANSI_RESET);
+        System.out.println(ANSI_BLUE +"\t\t\t\t         |    ______   DELIVERY   ______    | |          | |       "+ANSI_RESET);
+        System.out.println(ANSI_BLUE +"\t\t\t\t         |                                  | |            | |     "+ANSI_RESET);
+        System.out.println(ANSI_BLUE +"\t\t\t\t         |                                  | | |           | |    "+ANSI_RESET);
+        System.out.println(ANSI_BLUE +"\t\t\t\t         |__________________________________| |______________|_|   "+ANSI_RESET);
+        System.out.println(ANSI_BLUE +"\t\t\t\t         |                                                     |   "+ANSI_RESET);
+        System.out.println(ANSI_BLUE +"\t\t\t\t         |       0 0 O                           0 0 0         |   "+ANSI_RESET);
+        System.out.println(ANSI_BLUE +"\t\t\t\t         |-----0       0---------------------- 0       0-------|   "+ANSI_RESET);
+        System.out.println(ANSI_BLUE +"\t\t\t\t ________________0 0 0___________________________0 0 0_____________"+ANSI_RESET);
+        System.out.println(ANSI_BLUE +"\t\t\t\t __________________________________________________________________"+ANSI_RESET);
 
 
 
