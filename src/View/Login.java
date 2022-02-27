6f7d7605cc0b3f5381d14db5d2c09427b93dff0d
NezaBruce <nezabruce@gmail.com>
2022-02-27 20:07:19 +0200

    login

---
+++ src/View/Login.java
@@ -0,0 +1,15 @@
+package View;
+
+import java.util.Properties;
+
+public class Login {
+    public static void main(String[] args){
+        Properties properties = new Properties();
+//        properties.setProperty("Token", "bruce");
+//        properties.setProperty("UserId", "0");
+        System.out.println(properties.getProperty("Token"));
+    }
+
+
+
+}
