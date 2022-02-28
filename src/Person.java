5fb47ce54e70a2636048cf47308558a498f34e59
hasby-shanessa <umutoniwabohasby@gmail.com>
2022-02-28 13:09:10 +0200

    person

--- src/Person.java
+++ src/Person.java
@@ -5,4 +5,43 @@ public class Person {
     private String work;
     private String bname;
 
+    public String getFname() {
+        return fname;
+    }
+
+    public void setFname(String fname) {
+        this.fname = fname;
+    }
+
+    public String getLname() {
+        return lname;
+    }
+
+    public void setLname(String lname) {
+        this.lname = lname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getWork() {
+        return work;
+    }
+
+    public void setWork(String work) {
+        this.work = work;
+    }
+
+    public String getBname() {
+        return bname;
+    }
+
+    public void setBname(String bname) {
+        this.bname = bname;
+    }
 }
