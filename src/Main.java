8694058a1e137d447b29df7984b0228c03ddffc0
niyigenayves <wizynives@gmail.com>
2022-02-28 07:11:54 +0200

    Welcome

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+public class Main {
+    public static final String ANSI_BLUE = "\u001B[34m";
+    public static final String ANSI_RESET = "\u001B[0m";
+
+    public static void main(String[] args) {
+        System.out.println("\n                                                                                                                                         ");
+        
+        System.out.println(ANSI_BLUE +"\t     __                __    __________    __             _________       _______       ___           ___   _________         "+ANSI_RESET);
+        System.out.println(ANSI_BLUE +"\t    |  |              |  |  |          |  |  |           /          |   /   ___   \\    |   \\        /   |  |         |      "+ANSI_RESET);
+        System.out.println(ANSI_BLUE +"\t    |  |      __      |  |  |  |-------   |  |          |   --------   |   /   \\   |   |    \\      /    |  |  |------       "+ANSI_RESET);
+        System.out.println(ANSI_BLUE +"\t    |  |     /  \\     |  |  |  |          |  |          |  |           |  |     |  |   |     \\    /     |  |  |             "+ANSI_RESET);
+        System.out.println(ANSI_BLUE +"\t    |  |    /    \\    |  |  |  |          |  |          |  |           |  |     |  |   |  |\\  \\  /  /|  |  |  |            "+ANSI_RESET);
+        System.out.println(ANSI_BLUE +"\t    |  |   /  /\\  \\   |  |  |  |____      |  |          |  |           |  |     |  |   |  | \\  \\/  / |  |  |  |____       "+ANSI_RESET);
+        System.out.println(ANSI_BLUE +"\t    |  |  /  /  \\  \\  |  |  |       |     |  |          |  |           |  |     |  |   |  |  \\    /  |  |  |       |       "+ANSI_RESET);
+        System.out.println(ANSI_BLUE +"\t    |  | /  /    \\  \\ |  |  |  |----      |  |          |  |           |  |     |  |   |  |   \\__/   |  |  |  |----        "+ANSI_RESET);
+        System.out.println(ANSI_BLUE +"\t    |  |/  /      \\  \\|  |  |  |          |  |          |  |           |  |     |  |   |  |          |  |  |  |             "+ANSI_RESET);
+        System.out.println(ANSI_BLUE +"\t    |  /  /        \\  \\  |  |  |          |  |          |  |           |  |     |  |   |  |          |  |  |  |             "+ANSI_RESET);
+        System.out.println(ANSI_BLUE +"\t    |    /          \\    |  |   _______   |   ______    |  \\_______    |   \\___/   |   |  |          |  |  |   _____       "+ANSI_RESET);
+        System.out.println(ANSI_BLUE +"\t    |   /            \\   |  |          |  |          |   \\          |   \\         /    |  |          |  |  |         |     "+ANSI_RESET);
+        System.out.println(ANSI_BLUE +"\t     ---              ---    ----------    ----------      ---------      -------       --            --    ---------         "+ANSI_RESET);
+
+        System.out.println(ANSI_BLUE +"\t\t\t\t\t\t          _______   ______                 "+ANSI_RESET);
+        System.out.println(ANSI_BLUE +"\t\t\t\t\t\t         |__   __| |  __  |                "+ANSI_RESET);
+        System.out.println(ANSI_BLUE +"\t\t\t\t\t\t            | |    | |  | |                "+ANSI_RESET);
+        System.out.println(ANSI_BLUE +"\t\t\t\t\t\t            | |    | |  | |                "+ANSI_RESET);
+        System.out.println(ANSI_BLUE +"\t\t\t\t\t\t            | |    | |__| |                "+ANSI_RESET);
+        System.out.println(ANSI_BLUE +"\t\t\t\t\t\t             -      ------                 "+ANSI_RESET);
+
+
+    }
+}
