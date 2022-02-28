76bb09f51cf09e9bd920b8eafdbebe424ab1a22e
niyigenayves <wizynives@gmail.com>
2022-02-28 09:10:13 +0200

    App name

--- src/Main.java
+++ src/Main.java
@@ -5,26 +5,40 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("\n                                                                                                                                         ");
         
-        System.out.println(ANSI_BLUE +"\t     __                __    __________    __             _________       _______       ___           ___   _________         "+ANSI_RESET);
-        System.out.println(ANSI_BLUE +"\t    |  |              |  |  |          |  |  |           /          |   /   ___   \\    |   \\        /   |  |         |      "+ANSI_RESET);
-        System.out.println(ANSI_BLUE +"\t    |  |      __      |  |  |  |-------   |  |          |   --------   |   /   \\   |   |    \\      /    |  |  |------       "+ANSI_RESET);
-        System.out.println(ANSI_BLUE +"\t    |  |     /  \\     |  |  |  |          |  |          |  |           |  |     |  |   |     \\    /     |  |  |             "+ANSI_RESET);
-        System.out.println(ANSI_BLUE +"\t    |  |    /    \\    |  |  |  |          |  |          |  |           |  |     |  |   |  |\\  \\  /  /|  |  |  |            "+ANSI_RESET);
-        System.out.println(ANSI_BLUE +"\t    |  |   /  /\\  \\   |  |  |  |____      |  |          |  |           |  |     |  |   |  | \\  \\/  / |  |  |  |____       "+ANSI_RESET);
-        System.out.println(ANSI_BLUE +"\t    |  |  /  /  \\  \\  |  |  |       |     |  |          |  |           |  |     |  |   |  |  \\    /  |  |  |       |       "+ANSI_RESET);
-        System.out.println(ANSI_BLUE +"\t    |  | /  /    \\  \\ |  |  |  |----      |  |          |  |           |  |     |  |   |  |   \\__/   |  |  |  |----        "+ANSI_RESET);
-        System.out.println(ANSI_BLUE +"\t    |  |/  /      \\  \\|  |  |  |          |  |          |  |           |  |     |  |   |  |          |  |  |  |             "+ANSI_RESET);
-        System.out.println(ANSI_BLUE +"\t    |  /  /        \\  \\  |  |  |          |  |          |  |           |  |     |  |   |  |          |  |  |  |             "+ANSI_RESET);
-        System.out.println(ANSI_BLUE +"\t    |    /          \\    |  |   _______   |   ______    |  \\_______    |   \\___/   |   |  |          |  |  |   _____       "+ANSI_RESET);
-        System.out.println(ANSI_BLUE +"\t    |   /            \\   |  |          |  |          |   \\          |   \\         /    |  |          |  |  |         |     "+ANSI_RESET);
-        System.out.println(ANSI_BLUE +"\t     ---              ---    ----------    ----------      ---------      -------       --            --    ---------         "+ANSI_RESET);
+        System.out.println(ANSI_BLUE +"\t\t __                __    __________    __             _________       _______       ___           ___   _________    "+ANSI_RESET);
+        System.out.println(ANSI_BLUE +"\t\t|  |              |  |  |          |  |  |           /          |   /   ___   \\    |   \\        /   |  |         | "+ANSI_RESET);
+        System.out.println(ANSI_BLUE +"\t\t|  |      __      |  |  |  |-------   |  |          |   --------   |   /   \\   |   |    \\      /    |  |  |------  "+ANSI_RESET);
+        System.out.println(ANSI_BLUE +"\t\t|  |     /  \\     |  |  |  |          |  |          |  |           |  |     |  |   |     \\    /     |  |  |        "+ANSI_RESET);
+        System.out.println(ANSI_BLUE +"\t\t|  |    /    \\    |  |  |  |          |  |          |  |           |  |     |  |   |  |\\  \\  /  /|  |  |  |       "+ANSI_RESET);
+        System.out.println(ANSI_BLUE +"\t\t|  |   /  /\\  \\   |  |  |  |____      |  |          |  |           |  |     |  |   |  | \\  \\/  / |  |  |  |____  "+ANSI_RESET);
+        System.out.println(ANSI_BLUE +"\t\t|  |  /  /  \\  \\  |  |  |       |     |  |          |  |           |  |     |  |   |  |  \\    /  |  |  |       |  "+ANSI_RESET);
+        System.out.println(ANSI_BLUE +"\t\t|  | /  /    \\  \\ |  |  |  |----      |  |          |  |           |  |     |  |   |  |   \\__/   |  |  |  |----   "+ANSI_RESET);
+        System.out.println(ANSI_BLUE +"\t\t|  |/  /      \\  \\|  |  |  |          |  |          |  |           |  |     |  |   |  |          |  |  |  |        "+ANSI_RESET);
+        System.out.println(ANSI_BLUE +"\t\t|  /  /        \\  \\  |  |  |          |  |          |  |           |  |     |  |   |  |          |  |  |  |        "+ANSI_RESET);
+        System.out.println(ANSI_BLUE +"\t\t|    /          \\    |  |   _______   |   ______    |  \\_______    |   \\___/   |   |  |          |  |  |   _____  "+ANSI_RESET);
+        System.out.println(ANSI_BLUE +"\t\t|   /            \\   |  |          |  |          |   \\          |   \\         /    |  |          |  |  |         |"+ANSI_RESET);
+        System.out.println(ANSI_BLUE +"\t\t ---              ---    ----------    ----------      ---------      -------       --            --    ---------    "+ANSI_RESET);
 
-        System.out.println(ANSI_BLUE +"\t\t\t\t\t\t          _______   ______                 "+ANSI_RESET);
-        System.out.println(ANSI_BLUE +"\t\t\t\t\t\t         |__   __| |  __  |                "+ANSI_RESET);
-        System.out.println(ANSI_BLUE +"\t\t\t\t\t\t            | |    | |  | |                "+ANSI_RESET);
-        System.out.println(ANSI_BLUE +"\t\t\t\t\t\t            | |    | |  | |                "+ANSI_RESET);
-        System.out.println(ANSI_BLUE +"\t\t\t\t\t\t            | |    | |__| |                "+ANSI_RESET);
-        System.out.println(ANSI_BLUE +"\t\t\t\t\t\t             -      ------                 "+ANSI_RESET);
+        System.out.println(ANSI_BLUE +"\t\t\t\t\t\t\t          _______   ______                 "+ANSI_RESET);
+        System.out.println(ANSI_BLUE +"\t\t\t\t\t\t\t         |__   __| |  __  |                "+ANSI_RESET);
+        System.out.println(ANSI_BLUE +"\t\t\t\t\t\t\t            | |    | |  | |                "+ANSI_RESET);
+        System.out.println(ANSI_BLUE +"\t\t\t\t\t\t\t            | |    | |  | |                "+ANSI_RESET);
+        System.out.println(ANSI_BLUE +"\t\t\t\t\t\t\t            | |    | |__| |                "+ANSI_RESET);
+        System.out.println(ANSI_BLUE +"\t\t\t\t\t\t\t             -      ------                 "+ANSI_RESET);
+        
+        System.out.println(ANSI_BLUE +"\t\t\t\t __              ______         ________     ________       ________    __           "+ANSI_RESET);
+        System.out.println(ANSI_BLUE +"\t\t\t\t|  |           /         \\    /          |  |         \\    |        |  |  |        "+ANSI_RESET);
+        System.out.println(ANSI_BLUE +"\t\t\t\t|  |          |  / --- \\  |  |  / ------    |   ----\\  \\   |  ------   |  |       "+ANSI_RESET);
+        System.out.println(ANSI_BLUE +"\t\t\t\t|  |          |  |     |  |  |  |           |  |     \\  |  |  |        |  |         "+ANSI_RESET);
+        System.out.println(ANSI_BLUE +"\t\t\t\t|  |          |  |     |  |  |  |           |  |     |  |  |  |        |  |          "+ANSI_RESET);
+        System.out.println(ANSI_BLUE +"\t\t\t\t|  |          |  |     |  |  |  |    __     |  |     |  |  |  | ___    |  |          "+ANSI_RESET);
+        System.out.println(ANSI_BLUE +"\t\t\t\t|  |          |  |     |  |  |  |   |__ \\   |  |     |  |  |       |   |  |         "+ANSI_RESET);
+        System.out.println(ANSI_BLUE +"\t\t\t\t|  |          |  |     |  |  |  |     \\  |  |  |     |  |  |  -----    |  |         "+ANSI_RESET);
+        System.out.println(ANSI_BLUE +"\t\t\t\t|  |          |  |     |  |  |  |     |  |  |  |     /  |  |  |        |  |          "+ANSI_RESET);
+        System.out.println(ANSI_BLUE +"\t\t\t\t|   ______    |   \\___ /  |  |  \\___  /  |  |   ____/  /   |  |______  |   ______  "+ANSI_RESET);
+        System.out.println(ANSI_BLUE +"\t\t\t\t|          |   \\         /    \\         /   |         /    |         | |          |"+ANSI_RESET);
+        System.out.println(ANSI_BLUE +"\t\t\t\t __________      ------         ------       --------       ---------   __________   "+ANSI_RESET);
+
 
 
     }
