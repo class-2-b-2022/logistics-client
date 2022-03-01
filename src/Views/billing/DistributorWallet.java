77428073451c5a2fe1b499cf208efc25692e42ce
acele-happy <78531760+acele-happy@users.noreply.github.com>
2022-03-01 21:37:48 +0200

    added communication with server

--- src/Views/billing/DistributorWallet.java
+++ src/Views/billing/DistributorWallet.java
@@ -1,5 +1,11 @@
 package Views.billing;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.Scanner;
+import java.net.Socket;
+
 
 //save money, make transactions!!
 public class DistributorWallet {
@@ -7,7 +13,11 @@ public class DistributorWallet {
     public static final String ANSI_CYAN = "\u001B[36m";
     public static final String ANSI_RESET= "\u001B[0m";
     public static void Wallet(){
+
+
         int choice;
+        String choiceText="";
+
         Scanner scanner = new Scanner(System.in);
         System.out.println("\n\n");
         System.out.println("________________________________________"+ANSI_CYAN+"DISTRIBUTOR'S WALLET"+ANSI_RESET+"___________________________________________\n");
@@ -32,34 +42,47 @@ public class DistributorWallet {
         System.out.println("");
         System.out.print("                                     "+ANSI_GREEN +"||"+ANSI_RESET );
         System.out.print("                                     "+ANSI_GREEN +"||"+ANSI_RESET );
-//        System.out.println("");
 
 
-        System.out.println("\n\n");
-        System.out.println("                            WHAT DO YOU WANT TO DO WITH YOUR WALLET?                          ");
-        System.out.println("                    (1) Get my Balance                    ");
-        System.out.println("                    (2) Save to my Wallet                 ");
-        System.out.println("                    (3) Withdraw from my Wallet           ");
-        System.out.println(" Enter your choice: ");
-        choice = scanner.nextInt();
+        try {
+            Socket socket = new Socket("localhost", 2334);
+            OutputStream outToServer = socket.getOutputStream();
+            DataOutputStream requestToServer = new DataOutputStream(outToServer);
 
-        switch(choice){
-            case 1:
-                System.out.println("Total Balance of the Company");
-                //getBalance()
-                break;
-            case 2:
-                System.out.println("You successfully saved to your wallet");
-                //saveToWallet();
-                break;
-            case 3:
-                System.out.println("You successfully withdrawn from your wallet");
-                //withdrawFromWallet();
-                break;
-            default:
-                System.out.println("Please enter a valid choice");
+
+
+            System.out.println("\n\n");
+            System.out.println("                            WHAT DO YOU WANT TO DO WITH YOUR WALLET?                          ");
+            System.out.println("                    (1) Get my Balance                    ");
+            System.out.println("                    (2) Save to my Wallet                 ");
+            System.out.println("                    (3) Withdraw from my Wallet           ");
+            System.out.println(" Enter your choice: ");
+            choice = scanner.nextInt();
+
+
+            switch (choice) {
+                case 1:
+                    choiceText = "Get my Balance";
+                    break;
+                case 2:
+                    choiceText = "Save to my Wallet";
+                    break;
+                case 3:
+                    choiceText = "Withdraw from my Wallet";
+                    break;
+                default:
+                    System.out.println("Please enter a valid choice");
+            }
+
+            requestToServer.writeUTF(choiceText);
+
+            InputStream inFromServer = socket.getInputStream();
+            DataInputStream response = new DataInputStream(inFromServer);
+
+        }catch(Exception e){
+//            e.printStackTrace()
         }
-    }
+        }
 
     public static void main(String[] args){
         Wallet();
