778cbcb58a8ebf99605571044e37080ee8d9e102
gasaroleila <61538202+gasaroleila@users.noreply.github.com>
2022-03-09 10:46:25 +0200

    revamped code& fixed issues

--- src/logic/TestingServerConnecting.java
+++ src/logic/TestingServerConnecting.java
@@ -20,15 +20,15 @@ public class TestingServerConnecting {
         RequestBody clientRequest = new RequestBody();
         clientRequest.setRoute("/testing");
         clientRequest.setAction("testing");
-        BillingModel billModel = new BillingModel(userId, amount);
-        clientRequest.setData(billModel);
-        ObjectMapper objectMapper = new ObjectMapper();
-         ResponseBody res = new ConnectToServer().connectToServer(clientRequest);
-        System.out.println("Returned data " +res.getData());
-        JsonNode jsonNode = objectMapper.readTree(res.getData());
-        byte[] dataToparse = objectMapper.writeValueAsBytes(jsonNode);
-        BillingModel bills = objectMapper.readValue(dataToparse, BillingModel.class);
-        System.out.println(bills.getAmount());
+//        Wallet billModel = new Wallet(userId, amount);
+//        clientRequest.setData(billModel);
+//        ObjectMapper objectMapper = new ObjectMapper();
+//        ResponseBody res = new ConnectToServer().connectToServer(clientRequest);
+//        System.out.println("Returned data " +res.getData());
+//        JsonNode jsonNode = objectMapper.readTree(res.getData());
+//        byte[] dataToparse = objectMapper.writeValueAsBytes(jsonNode);
+//        BillingModel bills = objectMapper.readValue(dataToparse, BillingModel.class);
+//        System.out.println(bills.getAmount());
 
     }
 }
