20fd68b89424781e756c94e9884a005d6b0e000e
mugishakundasarah <mugishakundasarah@gmail.com>
2022-03-15 11:55:19 +0200

    added validation to inventory creation

--- src/Views/Inventory/Inventory.java
+++ src/Views/Inventory/Inventory.java
@@ -17,11 +17,13 @@ import logic.TestingServerConnecting;
 import main.views.inventory.Product;
 import models.ProductModel;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
 public class Inventory {
+    public static final String ANSI_RED = "\u001B[31m";
     public static final String ANSI_GREEN = "\u001B[32m";
     public static final String ANSI_CYAN = "\u001B[36m";
     public static final String ANSI_RESET= "\u001B[0m";
@@ -29,10 +31,19 @@ public class Inventory {
 
     ObjectMapper objectMapper = new ObjectMapper();
     ClientRequest clientRequest = new ClientRequest();
-    String json = "";
     ResponseBody responseBody;
+    String json = "";
 
-    public void viewProducts(int branchId){
+    int branchId;
+    InventoryModel inventoryModel = new InventoryModel();
+    Scanner scanner = new Scanner(System.in);
+
+    public void printWelcome(){
+        System.out.println("\n");
+        System.out.println("________________________________________"+ANSI_CYAN+"USER'S INVENTORY"+ANSI_RESET+"___________________________________________\n");
+    }
+    public List<Integer> viewProducts(int branchId){
+        List<Integer> productIds = new ArrayList<Integer>();
         try {
             clientRequest.setRoute("/products");
             clientRequest.setAction("GET");
@@ -47,17 +58,25 @@ public class Inventory {
             System.out.format("| #Id    | Product Name    | Product Type       | Company Id  | Price per Bulk |");
             System.out.format("+-----------------+------+%n");
             for ( ProductModel product : products) {
-
                 String line = String.format("%s    |  %s             |  %s          | %s         | %s        ",product.getProductId(),product.getProductName(),product.getProductType(),product.getCompanyId(), product.getPricePerBulk());
                 System.out.format(leftAlignFormat, line, 2, 3);
+
+                // insert the product id in the productIds array
+                productIds.add(product.getProductId());
             }
             System.out.format("+-----------------+---------------------+%n");
         }catch (Exception e){
             e.printStackTrace();
         }
+        finally {
+            return productIds;
+        }
     }
-    public void viewInventory(int branchId){
+    public void viewInventory(){
         try {
+            System.out.println("Enter your branch id to view Inventory: ");
+            branchId= scanner.nextInt();
+
             clientRequest.setRoute("/inventory");
             clientRequest.setAction("GET");
             clientRequest.setData(branchId);
@@ -82,117 +101,129 @@ public class Inventory {
             e.printStackTrace();
         }
     }
+    public void createInventory(){
+        try{
+            InventoryModel inventoryModel = new InventoryModel();
+
+            List<Integer> productIds;
+            int productId;
+            int status;
+            int branchId;
+
+            System.out.print("Enter your branch id to create Inventory: ");
+            branchId = scanner.nextInt();
+
+            System.out.print(" IN(0)/OUT?(1) ");
+            status = scanner.nextInt();
+
+            while(status != 0 && status != 1){
+                System.out.println(ANSI_RED + "Please give a valid status of your inventory" + ANSI_RESET);
+                System.out.print("status : ");
+                status = scanner.nextInt();
+            }
+
+            if(status == 0){
+                inventoryModel.setStatus("IN");
+            }else{
+                inventoryModel.setStatus("OUT");
+            }
+
+            productIds = this.viewProducts(branchId);
+            System.out.println("which product? ");
+            System.out.print("(Hint- Choose id from the listed products) : ");
+
+            productId = scanner.nextInt();
+
+            // Product Id validation
+            while(!productIds.contains(productId)){
+                System.out.println(ANSI_RED+ " Please give a valid productId " + ANSI_RESET);
+                System.out.print("ProductId : ");
+                productId = scanner.nextInt();
+            }
+            // End of product Id validation
+
+            inventoryModel.setProductId(productId);
+
+            System.out.println("How many products? ");
+            inventoryModel.setQuantity(scanner.nextInt());
+
+            // keep branch which registered the inventory
+            inventoryModel.setBranchId(branchId);
+
+            clientRequest.setRoute("/inventory");
+            clientRequest.setAction("POST");
+            clientRequest.setData(inventoryModel);
+            json = objectMapper.writeValueAsString(clientRequest);
+            responseBody = new ClientServerConnector().serverClientConnnector(json);
+            System.out.println(ANSI_GREEN + responseBody.getStatus() + ANSI_RESET);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+    }
+    public void updateInventory(){
+        try{
+            System.out.println("Enter your branch id to update Inventory: ");
+            branchId = scanner.nextInt();
+            clientRequest.setRoute("/inventory");
+            clientRequest.setAction("VIEW");
+            clientRequest.setData(branchId);
+            System.out.println("Here is the initial record and you need to update it.");
+            json = objectMapper.writeValueAsString(clientRequest);
+            responseBody = new ClientServerConnector().serverClientConnnector(json);
+            System.out.println(responseBody.getData());
+
+            System.out.println("Enter new quantity: ");
+            inventoryModel.setQuantity(scanner.nextInt());
+
+            clientRequest.setRoute("/inventory");
+            clientRequest.setAction("UPDATE");
+            clientRequest.setData(inventoryModel);
+            json = objectMapper.writeValueAsString(clientRequest);
+            responseBody = new ClientServerConnector().serverClientConnnector(json);
+            System.out.println(responseBody.getStatus());
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+    public void deleteInventory(){
+        try{
+            System.out.println("Enter your inventory id to delete Inventory: ");
+            int inventoryId = scanner.nextInt();
+            ClientRequest clientRequest2 = new ClientRequest();
+            clientRequest2.setRoute("/inventory");
+            clientRequest2.setAction("DELETE");
+            clientRequest2.setData(inventoryId);
+            json = objectMapper.writeValueAsString(clientRequest2);
+            responseBody = new ClientServerConnector().serverClientConnnector(json);
+            System.out.println("Message: " + responseBody.getMessage() + " status: " +responseBody.getStatus() + " data: "+ responseBody.getData());
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
     public static void Inventory() throws Exception {
-
-        ObjectMapper objectMapper = new ObjectMapper();
-        ClientRequest clientRequest = new ClientRequest();
-        String json = "";
-        ResponseBody responseBody;
-
+        Inventory inventory = new Inventory();
         int choice;
-        int branchId;
-
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("\n\n");
-        System.out.println("________________________________________"+ANSI_CYAN+"USER'S INVENTORY"+ANSI_RESET+"___________________________________________\n");
-
-        System.out.print(ANSI_GREEN+"                                     ||"+ANSI_RESET );
-        System.out.print(ANSI_GREEN+"                                     ||"+ANSI_RESET );
-        System.out.println("");
-        System.out.print("                                 ~  ~"+ANSI_GREEN +"||"+ANSI_RESET+"  ~");
-        System.out.print("                              ~  ~"+ANSI_GREEN +"||"+ANSI_RESET+"  ~");
-        System.out.println("");
-        System.out.print("                                ~               ");
-        System.out.print("                       ~               ");
-        System.out.println("");
-        System.out.print("                                 ~  ~"+ANSI_GREEN +"||"+ANSI_RESET +  "~");
-        System.out.print("                                ~  ~"+ANSI_GREEN +"||"+ANSI_RESET +  "~");
-        System.out.println("");
-        System.out.print("                                         ~");
-        System.out.print("                                       ~");
-        System.out.println("");
-        System.out.print("                                ~  ~"+ANSI_GREEN +" ||"+ANSI_RESET +"  ~");
-        System.out.print("                             ~  ~"+ANSI_GREEN +" ||"+ANSI_RESET +"  ~");
-        System.out.println("");
-        System.out.print("                                     "+ANSI_GREEN +"||"+ANSI_RESET );
-        System.out.print("                                     "+ANSI_GREEN +"||"+ANSI_RESET );
-
-
-
+        inventory.printWelcome();
         try {
-
-            System.out.println("\n\n");
             System.out.println("                    WHAT DO YOU WANT TO DO WITH YOUR INVENTORY?                          ");
             System.out.println("                    (1) Create Inventory                    ");
             System.out.println("                    (2) Get Inventory Details                    ");
             System.out.println("                    (3) Update my Inventory                 ");
             System.out.println("                    (4) Delete from my Inventory           ");
             System.out.print(" Enter your choice: ");
-            choice = scanner.nextInt();
-            InventoryModel inventoryModel = new InventoryModel();
-            Inventory inventory = new Inventory();
+            choice = inventory.scanner.nextInt();
             switch (choice) {
                 case 1:
-                    System.out.println("Enter your branch id to create Inventory: ");
-                    branchId = scanner.nextInt();
-                    inventory.viewProducts(branchId);
-//                    System.out.println("IN/OUT? ");
-                    inventoryModel.setStatus("IN");
-
-                    System.out.println("which product? ");
-                    System.out.print("(Hint: Choose id from given products) :: ");
-                    inventoryModel.setProductId(scanner.nextInt());
-
-                    System.out.println("How many products? ");
-                    inventoryModel.setQuantity(scanner.nextInt());
-
-                    // set user who registered inventory
-                    inventoryModel.setBranchId(branchId);
-
-                    clientRequest.setRoute("/inventory");
-                    clientRequest.setAction("POST");
-                    clientRequest.setData(inventoryModel);
-                    json = objectMapper.writeValueAsString(clientRequest);
-                    responseBody = new ClientServerConnector().serverClientConnnector(json);
-                    System.out.println(responseBody.getStatus());
+                    inventory.createInventory();
                     break;
                 case 2:
-
-                    System.out.println("Enter your branch id to view Inventory: ");
-                    branchId= scanner.nextInt();
-                    inventory.viewInventory(branchId);
+                    inventory.viewInventory();
                     break;
                 case 3:
-                    System.out.println("Enter your branch id to update Inventory: ");
-                    branchId = scanner.nextInt();
-                    clientRequest.setRoute("/inventory");
-                    clientRequest.setAction("VIEW");
-                    clientRequest.setData(branchId);
-                    System.out.println("Here is the initial record and you need to update it.");
-                    json = objectMapper.writeValueAsString(clientRequest);
-                    responseBody = new ClientServerConnector().serverClientConnnector(json);
-                    System.out.println(responseBody.getData());
-
-                    System.out.println("Enter new quantity: ");
-                    inventoryModel.setQuantity(scanner.nextInt());
-
-                    clientRequest.setRoute("/inventory");
-                    clientRequest.setAction("UPDATE");
-                    clientRequest.setData(inventoryModel);
-                    json = objectMapper.writeValueAsString(clientRequest);
-                    responseBody = new ClientServerConnector().serverClientConnnector(json);
-                    System.out.println(responseBody.getStatus());
-                    break;
+                    inventory.updateInventory();
                 case 4:
-                    System.out.println("Enter your inventory id to delete Inventory: ");
-                    int inventoryId = scanner.nextInt();
-                    ClientRequest clientRequest2 = new ClientRequest();
-                    clientRequest2.setRoute("/inventory");
-                    clientRequest2.setAction("DELETE");
-                    clientRequest2.setData(inventoryId);
-                    json = objectMapper.writeValueAsString(clientRequest2);
-                    responseBody = new ClientServerConnector().serverClientConnnector(json);
-                    System.out.println("Message: " + responseBody.getMessage() + " status: " +responseBody.getStatus() + " data: "+ responseBody.getData());
+                    inventory.deleteInventory();
                     break;
                 default:
                     System.out.println("Please enter a valid choice");
