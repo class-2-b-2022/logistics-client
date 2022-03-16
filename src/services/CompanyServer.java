91812e15afa773ee309f77d0ac28d40132ef06b8
Alliance-mpano <alliekook03@gmail.com>
2022-03-16 07:18:32 +0200

    Modified companytypes

--- src/services/CompanyServer.java
+++ src/services/CompanyServer.java
@@ -26,11 +26,17 @@ public class CompanyServer {
                 BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream(), StandardCharsets.UTF_8));
 
                 String line = reader.readLine();
+                System.out.println(line);
                 JSONObject jsonObject = new JSONObject(line);
+                String companyName = jsonObject.getString("CompanyName");
+                String companyDescription = jsonObject.getString("CompanyDescription");
+                String companyEmail = jsonObject.getString("CompanyEmail");
+                String companyType = jsonObject.getString("CompanyType");
+                String companyPhone = jsonObject.getString("CompanyPhone");
 
                 writer.write(jsonObject + "\n");
                 writer.flush();
-                System.out.println("sending to from server \n" + jsonObject);
+//                System.out.println("sending to from server \n" + jsonObject);
 
 
             } catch (IOException e) {
